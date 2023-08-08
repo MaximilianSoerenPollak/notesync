7a1a83a7ecf48e7173f8636ed959d51df810d9f5
Maximilian <74557518+MaximilianSoerenPollak@users.noreply.github.com>
2023-08-08 12:10:20 +0000

    Feature: v0.1 MVP Finished

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.18
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	golang.org/x/exp v0.0.0-20230807204917-050eac23e9de // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
 )
 
-// Step 1. We have to get the folder and check for updates on it?
-
+// Where are all the notes we want to track
 var notesFolder = "../../Documents/Maximilians Brain"
-
-// Step 2. Define file structure for the json we want to save
 
 type fileStruct struct {
 	FileName   string `json:"filename,omitempty"`
@@ -22,22 +22,24 @@ type fileStruct struct {
 }
 
 func createFile(fileName string) error {
-	_, err := os.Create(fileName)
+	_, err := os.OpenFile(fileName, os.O_CREATE, 0644)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("Creating file: ", fileName)
+		log.Println(err)
 	}
 	return nil
 }
-func openFile(fileName string) ([]fileStruct, error) {
+
+func openAndReadFile(fileName string) ([]fileStruct, error) {
 	jsonFile, err := os.Open(fileName)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return nil, err
 	}
 	defer jsonFile.Close()
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return nil, err
 	}
 	files := []fileStruct{}
@@ -45,72 +47,163 @@ func openFile(fileName string) ([]fileStruct, error) {
 	return files, nil
 }
 
-func writeFileStructToFile(fs fileStruct, fileName string) error {
-	// fmt.Println("This is a file struct", fs)
-	files, err := openFile(fileName)
+func checkIfFileExists(fileName string) ([]fileStruct, error) {
+	files, err := openAndReadFile(fileName)
 	if err != nil {
-		if os.IsNotExist(err){
-			fmt.Println("File does not exists, creating it now")
+		if errors.Is(err, fs.ErrNotExist) {
+			log.Println("File does not exists, creating it now")
 			err := createFile(fileName)
 			if err != nil {
-				fmt.Println("Error creating File")
-				return err 
+				log.Println("Error creating File: ", fileName)
+				return nil, err
 			}
-			writeFileStructToFile(fs, fileName)
+			files, err := checkIfFileExists(fileName)
+			if err != nil {
+				log.Println("Error checking File: ", fileName)
+				return nil, err
+			}
+			return files, nil
 		}
-		fmt.Println(err)
-		return err 
+		log.Println("Error reading file: ", fileName)
+		log.Println(err)
+		return nil, err
 	}
+	return files, nil
+}
+
+func writeFileStructToFile(fs fileStruct, fileName string) error {
+	files, err := checkIfFileExists(fileName)
 	if err != nil {
-		fmt.Println("File potentially not yet created, creating File")
-		err = createFile(fileName)
-		if err != nil {
-			fmt.Println(err)
-		}
+		log.Println(err)
+		return err
 	}
 	files = append(files, fs)
 	data, err := json.MarshalIndent(files, "", " ")
 	if err != nil {
 		fmt.Println("Could not marshal data")
-		fmt.Println(err)
+		log.Println(err)
 		return err
 	}
 	err = ioutil.WriteFile(fileName, data, 0644)
 	if err != nil {
-		fmt.Println("Could write data to file")
-		fmt.Println(err)
+		fmt.Println("Could not write data to file")
+		log.Println(err)
 		return err
 	}
 	return nil
 }
 
-func findFiles(folder string) {
+func findFiles(folder string) error {
 	err := filepath.Walk(folder,
 		func(_ string, info os.FileInfo, err error) error {
 			// We don't need to track obsidian config files
 			if info.IsDir() && strings.HasPrefix(info.Name(), ".obsidian") {
 				return filepath.SkipDir
 			}
-			fmt.Println("This is info.name", info.Name())
-			fmt.Println("This is info.Modtime", info.ModTime())
 			newFile := fileStruct{
 				FileName:   info.Name(),
 				ModifiedAt: info.ModTime().String(),
 			}
 			writeFileStructToFile(newFile, "temp.json")
 			if err != nil {
+				log.Println(err)
 				return err
 			}
 			return nil
 		})
 	if err != nil {
 		log.Println(err)
+		return err
 	}
+	return nil
 }
 
-func compareFiles(){}
+func checkIfFileIsEmpty(fileName string) (bool, error) {
+	fileInfo, err := os.Stat(fileName)
+	if err != nil {
+		log.Println("Could not read filesize of file: ", fileName)
+		log.Println(err)
+		return false, err
+	}
+	if fileInfo.Size() == 0 {
+		return true, nil
+	} else {
+		return false, nil
+	}
 
+}
+func replaceTrackedNotesFile() error {
+	err := os.Remove("trackednotes.json")
+	if err != nil {
+		log.Println("Could not remove trackednotes.json")
+		log.Println(err)
+		return err
+	}
+	os.Rename("temp.json", "trackednotes.json")
+	log.Println("Renamed temp.json -> trackednotes.json")
+	return nil
+}
+
+func compareFiles() error {
+	// Temp.json should already exists due to the called functions before
+	temp_files, err := openAndReadFile("temp.json")
+	if err != nil {
+		log.Println("could not open File: temp.json")
+		return err
+	}
+	// Perm.json could be not created yet. Therefore we ned to check
+	tracked_notes, err := checkIfFileExists("trackednotes.json")
+	if err != nil {
+		log.Println("could not open File: trackednotes.json")
+		return err
+	}
+	log.Println("Checking if trackednotes.json is empty")
+	fileEmpty, err := checkIfFileIsEmpty("trackednotes.json")
+	if err != nil {
+		log.Println("could not check File for size: trackednotes.json")
+		return err
+	}
+	if fileEmpty || !reflect.DeepEqual(temp_files, tracked_notes) {
+		err := replaceTrackedNotesFile()
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		return nil
+	}
+	log.Println("Files are equal. Removing unneeded temp.json")
+	err = os.Remove("temp.json")
+	if err != nil {
+		log.Println("Something went wrong deleting temp.json .")
+		log.Fatal(err)
+	}
+	return nil
+}
 
 func main() {
-	findFiles(notesFolder)
+	LOG_FILE := "notesync.log"
+	logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.Printf("=========EXECUTING============")
+	err = createFile("temp.json")
+	if err != nil {
+		log.Println("Something went wrong creating temp.json .")
+		log.Fatal(err)
+	}
+	err = findFiles(notesFolder)
+	if err != nil {
+		log.Println("Something went wrong finding Files.")
+		log.Fatal(err)
+	}
+	err = compareFiles()
+	if err != nil {
+		log.Println("Something went wrong comparing Files.")
+		log.Fatal(err)
+	}
+	log.Printf("=========STOPPING EXECUTION =========")
 }
